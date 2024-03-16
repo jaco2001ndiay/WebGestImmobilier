d8d8961b7799041feb8ba62ca79c9d6694ad1dfc
Jacoelmaestro <jaco2001.com@gmail.com>
2024-03-16 14:22:57 +0000

    final commit

--- Models/Terrain.cs
+++ Models/Terrain.cs
@@ -4,19 +4,20 @@ using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using WebGestImmobilier.Models.App_LocalResources;
 namespace WebGestImmobilier.Models
 { 
     public class Terrain
     {
         [Key]
         public int Idterrain { get; set; }
-        [Display(Name = "Description du terrain "), Required(ErrorMessage = "*"), MaxLength(1000, ErrorMessage = " La taille max est de 1000")]
+        [Display(Name = "land_id", ResourceType =typeof(ResourceFrc)), Required(ErrorMessage = "*"), MaxLength(1000, ErrorMessage = " La taille max est de 1000")]
         public string Description { get; set; }
-        [Display(Name = "Prix du produit")]
+        [Display(Name = "land_description", ResourceType = (typeof(ResourceFrc)))]
         public int Prix { get; set; }
-        [Display(Name = "Superficie du bien"), Required(ErrorMessage = "*")]
+        [Display(Name = "land_price", ResourceType =typeof(ResourceFrc)), Required(ErrorMessage = "*")]
         public float? Superficie { get; set; }
-        [Display(Name = "Localisation du bien "), Required(ErrorMessage = "*"), MaxLength(300, ErrorMessage = " La taille max est de 1000")]
+        [Display(Name = "land_area", ResourceType =typeof(ResourceFrc)), Required(ErrorMessage = " *"), MaxLength(300, ErrorMessage = " La taille max est de 1000")]
         public string Localisation { get; set; }
         public int? IdProprio { get; set; }
         [ForeignKey("IdProprio")]
@@ -26,7 +27,7 @@ namespace WebGestImmobilier.Models
 
 
 
-        [Display(Name = "type de terrain "), Required(ErrorMessage = "*")]
+        [Display(Name = "land_type",ResourceType =typeof(ResourceFrc)), Required(ErrorMessage = "*")]
         public int typeterrain { get; set; }
 
      //   public virtual Bien bien { get; set; } 
